a65ab59dac3f859187deec32d3c8f4efbd603204
naren2631 <narendrababu2631@gmail.com>
2024-09-30 16:19:20 +0530

    terraform file

--- main.tf
+++ main.tf
@@ -1,1 +1,15 @@
-# resource creation
+
+terraform {
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "~> 4.2.0"
+    }
+  }
+}
+
+# Resource Creation
+resource "azurerm_resource_group" "example" {
+  name = "example-rg"
+  location = "east US"
+}
